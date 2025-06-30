b3116e2d6a5bd9dc578794913d80e566b0610fdb
muhammedjazool <muhammedjazool47@gmail.com>
2025-06-30 19:57:33 +0530

    Finished My Work

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 
 const navDialog = document.getElementById('nav-dialog');
 function handleMenu() {
-    navDialog.classList.toggle('hidden');
+     navDialog.classList.toggle('hidden');
 }
 
-const initialTranslateLTR = -48*4;
-const initialTranslateRTL = 36*4;
+const initialTranslateLTR = -48 * 4;
+const initialTranslateRTL = 36 * 4;
 
 function setupIntersectionObserver(element, isLTR, speed) {
-    const intersectionCallback = (entries) => {
-        const isIntersecting = entries[0].isIntersecting;
-        if(isIntersecting) {
-            document.addEventListener('scroll', scrollHandler);
-        } else {
-            document.removeEventListener('scroll', scrollHandler);
-        }
-    }
-    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+     const intersectionCallback = (entries) => {
+          const isIntersecting = entries[0].isIntersecting;
+          if (isIntersecting) {
+               document.addEventListener('scroll', scrollHandler);
+          } else {
+               document.removeEventListener('scroll', scrollHandler);
+          }
+     }
+     const intersectionObserver = new IntersectionObserver(intersectionCallback);
 
-    intersectionObserver.observe(element);
+     intersectionObserver.observe(element);
 
-    function scrollHandler() {
-        const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
+     function scrollHandler() {
+          const translateX = (window.innerHeight - element.getBoundingClientRect().top) * speed;
 
-        let totalTranslate = 0;
-        if(isLTR) {
-            totalTranslate = translateX + initialTranslateLTR;
-        } else {
-            totalTranslate = -(translateX + initialTranslateRTL);
-        }
+          let totalTranslate = 0;
+          if (isLTR) {
+               totalTranslate = translateX + initialTranslateLTR;
+          } else {
+               totalTranslate = -(translateX + initialTranslateRTL);
+          }
 
-        element.style.transform = `translateX(${totalTranslate}px)`;
-    }
+          element.style.transform = `translateX(${totalTranslate}px)`;
+     }
 
 }
 
@@ -48,14 +48,14 @@ setupIntersectionObserver(line4, true, 0.8);
 
 const dtElements = document.querySelectorAll('dt');
 dtElements.forEach(element => {
-    element.addEventListener('click', () => {
-        const ddId = element.getAttribute('aria-controls');
-        const ddElement = document.getElementById(ddId);
-        const ddArrowIcon = element.querySelectorAll('i')[0];
+     element.addEventListener('click', () => {
+          const ddId = element.getAttribute('aria-controls');
+          const ddElement = document.getElementById(ddId);
+          const ddArrowIcon = element.querySelectorAll('i')[0];
 
-        ddElement.classList.toggle('hidden');
-        ddArrowIcon.classList.toggle('-rotate-180');
-    })
+          ddElement.classList.toggle('hidden');
+          ddArrowIcon.classList.toggle('-rotate-180');
+     })
 })
 
 
